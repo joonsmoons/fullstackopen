e27eda6419bbd67351e8804234364d0b53c9d49a
Justin Lee <joonsmoons@gmail.com>
2022-09-26 19:07:24 +0900

    add data for single country search functionality

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,10 +4,6 @@ import axios from 'axios'
 function App() {
   const [filter, setFilter] = useState('')
   const [countries, setCountries] = useState([])
-  const [tooMany, setTooMany] = useState('')
-  // useEffect(() => {
-  //   axois.get()
-  // })
 
   useEffect(() => {
     // console.log('effect')
@@ -20,21 +16,42 @@ function App() {
 
   const handleFilter = (event) => {
     setFilter(event.target.value)
-    console.log(filteredCountries.length)
   }
 
-  const filteredCountries = countries.filter(country => {
-    return country.name.common.toLowerCase().includes(filter.toLowerCase())
-  })
-
+  let filteredCountries = countries
+    .filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
+  
+  const showFilter = () => {
+    // console.log('length:', filteredCountries.length)
+    if (filteredCountries.length > 10) {
+      return 'Please specify another filter. Too many matches (10+).'
+    } else if (filteredCountries.length > 1) {
+      return filteredCountries.map(country => <div key={country.cca3}>{country.name.common}</div>)
+    } else if (filteredCountries.length === 1) {
+      const country = filteredCountries[0]
+      console.log(country)
+      console.log(country.languages)
+      return (
+        <>
+          <h1>{country.name.common}</h1>
+          <div>capital: {country.capital[0]}</div>
+          <div>area: {country.area}</div>
+          <h2>languages:</h2>
+          <ul>
+            {Object.values(country.languages).map(language => <li>{language}</li>)}
+          </ul>
+          <img src={country.flags.png}/>
+        </>
+      )
+    }
+  }
 
   return (
     <div className="App">
       <div>find countries <input value={filter} onChange={handleFilter}/></div>
-      {filteredCountries.map(country => <div key={country.cca3}>{country.name.common}</div>)}
+      {showFilter()}
     </div>
-    
-  );
+  )
 }
 
 export default App;
