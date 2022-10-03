a5a3a5d1740d65510c3939e016b7445f927730ef
Justin Lee <joonsmoons@gmail.com>
2022-10-03 13:55:31 +0900

    change url link to deploy to internet, add proxy

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,29 +1,29 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+import axios from "axios";
+const baseUrl = "/api/persons";
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-const create = newObj => {
-    const request = axios.post(baseUrl, newObj)
-    return request.then(response => response.data)
-}
+const create = (newObj) => {
+  const request = axios.post(baseUrl, newObj);
+  return request.then((response) => response.data);
+};
 
-const remove = id => {
-    const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then(response => response.data)
-}
+const remove = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
 
 const update = (id, changeObj) => {
-    const request = axios.put(`${baseUrl}/${id}`, changeObj)
-    return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, changeObj);
+  return request.then((response) => response.data);
+};
 
-export default { 
-    getAll, 
-    create, 
-    remove, 
-    update
-}
+export default {
+  getAll,
+  create,
+  remove,
+  update,
+};
